7392650d26fe4e3aa7172ecabcb7b432697ffd7e
vova-ivanov <vova@pulumi.com>
2025-09-30 09:29:46 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+
+// Simple configuration value
+const config = new pulumi.Config();
+const environment = config.get("environment") || "dev";
+
+// Export some simple outputs
+export const message = `Example output, stack name: ${environment}!`;
+export const timestamp = `Current time: ${new Date().toISOString()}`;
