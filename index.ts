a6af4dbd6f2d37c8f7706ce35c4685d54ecf17d1
Neo <neo@pulumi.com>
2025-10-02 23:34:59 +0000

    preview changes - neo-preview-1759448099374

--- index.ts
+++ index.ts
@@ -7,3 +7,5 @@ const environment = config.get("environment") || "dev";
 // Export some simple outputs
 export const message = `Example output, stack name: ${environment}!`;
 export const timestamp = `Current time: ${new Date().toISOString()}`;
+export const iteration = "First iteration test";
+export const secondTest = "Second iteration test";
