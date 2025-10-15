de9b7ccb2f8b9f455fe536ae85fcf25584a6c905
vova-ivanov <vova@pulumi.com>
2025-10-15 12:33:18 -0700

    Added gitignore

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as command from "@pulumi/command";
 
 // Simple configuration value
 const config = new pulumi.Config();
-const environment = config.get("environment") || "dev";
+const environment = config.get("environment");
+
+// Resource that takes 10 seconds to create using a sleep command
+const slowResource = new command.local.Command("slowResource", {
+    create: "sleep 30 && echo 'Created 2'",
+});
 
 // Export some simple outputs
 export const message = `Example output, stack name: ${environment}!`;
 export const timestamp = `Current time: ${new Date().toISOString()}`;
+export const abc = "124";
+export const slowResourceOutput = slowResource.stdout;
