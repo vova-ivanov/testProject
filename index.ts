aa194ad5b1bd9c3c6ead2cef82b7937493ffff16
Neo <neo@pulumi.com>
2025-12-23 21:03:56 +0000

    preview changes - neo-preview-1766523836049

--- index.ts
+++ index.ts
@@ -15,3 +15,5 @@ export const message = `Example output, stack name: ${environment}!`;
 export const timestamp = `Current time: ${new Date().toISOString()}`;
 export const abc = "124";
 export const slowResourceOutput = slowResource.stdout;
+export const testLine4 = "Fourth test line added after restart";
+export const testLine5 = "Fifth test line added after restart";
