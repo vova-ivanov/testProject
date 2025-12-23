c8112ccfee5549c4a2d5daafa30a2f9c32344641
Neo <neo@pulumi.com>
2025-12-23 23:35:11 +0000

    preview changes - neo-preview-1766532911189

--- index.ts
+++ index.ts
@@ -14,4 +14,5 @@ const slowResource = new command.local.Command("slowResource", {
 export const message = `Example output, stack name: ${environment}!`;
 export const timestamp = `Current time: ${new Date().toISOString()}`;
 export const abc = "124";
+// This is a test line added for preview testing
 export const slowResourceOutput = slowResource.stdout;
